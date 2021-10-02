ade1f36aacb5c3b5e7a6943e23e5ddfafbebb26a
ThFigueiredo <thfigueiredo.developer@gmail.com>
2021-10-02 09:38:26 -0300

    AULA - Editando cadastro com Thymeleaf e Spring Boot

--- src/main/java/curso/springboot/api/controller/PessoaController.java
+++ src/main/java/curso/springboot/api/controller/PessoaController.java
@@ -4,9 +4,13 @@ import curso.springboot.domain.model.PessoaModel;
 import curso.springboot.domain.repository.PessoaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
 
 @Controller
 public class PessoaController {
@@ -16,8 +20,10 @@ public class PessoaController {
 
     //SALVAR (MÉTODO DE REDIRECIONAMENTO)
     @RequestMapping(method = RequestMethod.GET, value = "/cadastropessoa")
-    public String inicio() { //início = método
-        return "cadastro/cadastropessoa";
+    public ModelAndView inicio() { //início = método
+        ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
+        modelAndView.addObject("pessoaobj", new PessoaModel());
+        return modelAndView;
     }
 
 //    //SALVAR (SALVA REDIRECIONANDO PRA MESMA PASTA)
@@ -27,23 +33,36 @@ public class PessoaController {
 //        return "cadastro/cadastropessoa";
 //    }
     //SALVAR (SALVA E CARREGA O LISTAR NA MESMA PÁGINA)
-    @RequestMapping(method = RequestMethod.POST, value = "/salvarpessoa")
+    @RequestMapping(method = RequestMethod.POST, value = "**/salvarpessoa") //** ignora o que vem antes na url
     public ModelAndView salvar(PessoaModel pessoaModel) {
         pessoaRepository.save(pessoaModel);
 
         ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
         Iterable<PessoaModel> pessoaIt = pessoaRepository.findAll();
         andView.addObject("pessoas", pessoaIt);
+        andView.addObject("pessoaobj", new PessoaModel()); //passando objeto vazio
 
         return andView;
     }
-
     //LISTAR
     @RequestMapping(method = RequestMethod.GET, value = "/listarpessoas")
     public ModelAndView pessoas() {
         ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
         Iterable<PessoaModel> pessoaIt = pessoaRepository.findAll();
         andView.addObject("pessoas", pessoaIt); //pessoas faz interação com thymeleaf
+        andView.addObject("pessoaobj", new PessoaModel());
+
         return andView;
+    }
+
+    @GetMapping("/editarpessoa/{idpessoa}") //GetMapping subistitui o @RequestMapping acima
+    public ModelAndView editar(@PathVariable("idpessoa") Long idpessoa) {//idpessoas faz interação com thymeleaf
+
+        Optional<PessoaModel> pessoaModel = pessoaRepository.findById(idpessoa); //instanciando o objeto pessoa
+
+        ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
+        modelAndView.addObject("pessoaobj", pessoaModel.get());
+        return modelAndView;
+
     }
 }
