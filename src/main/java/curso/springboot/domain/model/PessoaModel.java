e799e2930f8b4582bfd8d0f12d0f7d37f6a34f47
ThFigueiredo <thfigueiredo.developer@gmail.com>
2021-10-05 13:46:47 -0500

    AULA - Consumindo Web Service RESTful CEP (https://viacep.com.br/)

--- src/main/java/curso/springboot/domain/model/PessoaModel.java
+++ src/main/java/curso/springboot/domain/model/PessoaModel.java
@@ -16,6 +16,7 @@ public class PessoaModel implements Serializable {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+
     @NotNull(message="Nome não pode ser nulo")
     @NotEmpty(message = "Nome não pode ser vazio")
     private String nome;
@@ -26,6 +27,63 @@ public class PessoaModel implements Serializable {
 
     @Min(value = 18, message = "Idade inválida")
     private int idade;
+
+    private String cep;
+    private String rua;
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public String getIbge() {
+        return ibge;
+    }
+
+    public void setIbge(String ibge) {
+        this.ibge = ibge;
+    }
+
+    private String bairro;
+    private String cidade;
+    private String uf;
+    private String ibge;
+
 
     public String getQuaquer() {
         return quaquer;
